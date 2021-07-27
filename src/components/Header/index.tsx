29f425e3e3a6080933ae5b099c281581dbcb37ac
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-07-26 21:16:29 -0300

    Adicionando React Modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,16 @@
 import Logo from '../../assets/logo.svg';
 import { Container, Content } from './styles';
 
-export function Header() {
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void
+}
+
+export function Header(props: HeaderProps) {
   return (
     <Container>
       <Content>
         <img src={Logo} alt="dt money" />
-        <button type="button">
+        <button type="button" onClick={props.onOpenNewTransactionModal} >
           Nova Transação
         </button>
       </Content>
