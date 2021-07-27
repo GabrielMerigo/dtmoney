8bbeecca8b4e81dbc30ceb3320241fe084e02753
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-07-26 21:39:15 -0300

    more accs

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
 import Modal from "react-modal";
 import { useState } from "react";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
 Modal.setAppElement('#root');
 
@@ -20,15 +21,12 @@ export function App() {
   return (
     <>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
-      <Dashboard />
-
-      <Modal
+      <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar Transação</h2>
-      </Modal>
-      <GlobalStyle/>
+      />
+      <Dashboard />
+      <GlobalStyle />
     </>
   );
 }
