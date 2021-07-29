02cf520ca1463c36c9c8c0c18c0697e841b12da5
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-07-28 22:33:53 -0300

    Entendendo contexto

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,12 +1,23 @@
+import { useEffect, useState } from "react";
+import { api } from "../../services/api";
 import { Container } from "../Dashboard/styles";
 import { Summary } from "../Summary";
 import { TransactionsTable } from "../TransactionsTable";
+
+interface Transaction {
+  id: number;
+  title: string;
+  amount: number;
+  type: string;
+  category: string;
+  createdAt: string;
+}
 
 export function Dashboard() {
   return (
     <Container>
       <Summary />
-      <TransactionsTable />
+      <TransactionsTable/>
     </Container>
   )
 }
