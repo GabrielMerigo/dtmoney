c4f5c143773d762717a765ccaf836173e89e75e5
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-07-28 22:46:46 -0300

    Update index.tsx

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,22 +1,11 @@
-import { useEffect, useState } from "react";
-import { api } from "../../services/api";
 import { Container } from "../Dashboard/styles";
 import { Summary } from "../Summary";
 import { TransactionsTable } from "../TransactionsTable";
 
-interface Transaction {
-  id: number;
-  title: string;
-  amount: number;
-  type: string;
-  category: string;
-  createdAt: string;
-}
-
 export function Dashboard() {
   return (
     <Container>
-      <Summary />
+      <Summary/>
       <TransactionsTable/>
     </Container>
   )
